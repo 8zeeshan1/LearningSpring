6c3959f2a6b60241228c67dd03ec5801ccf43a76
MOHD Zeeshan Quraishi <zq8281@gmail.com>
2026-07-04 21:50:54 +0530

    Created a repository package to store the users in memory

--- src/main/java/com/demo/demo/repositories/UsersRepo.java
+++ src/main/java/com/demo/demo/repositories/UsersRepo.java
@@ -1,4 +1,21 @@
 package com.demo.demo.repositories;
 
+import com.demo.demo.dto.UserDto;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
 public class UsersRepo {
+    List<UserDto> users = new ArrayList<>();
+
+    public UsersRepo(){
+        users.add(new UserDto("salama101", "zq8281@gmail.com", "Zeeshan"));
+        users.add(new UserDto("lalua202", "lallilal@gmail.com", "Lalu"));
+        users.add(new UserDto("chattis36", "kamles@gmail.com", "Kamal Singh"));
+    }
+
+    public List<UserDto> findAll(){
+        return users;
+    }
 }
