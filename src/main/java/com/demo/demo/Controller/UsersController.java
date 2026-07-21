987611b6e8e9ae01146d78a96e5203de7bb0c3bc
MOHD Zeeshan Quraishi <zq8281@gmail.com>
2026-07-21 19:42:20 +0530

    Added more controllers

--- src/main/java/com/demo/demo/Controller/UsersController.java
+++ src/main/java/com/demo/demo/Controller/UsersController.java
@@ -1,4 +1,34 @@
 package com.demo.demo.Controller;
+import java.util.List;
 
+import com.demo.demo.dto.CreateUserDTO;
+import com.demo.demo.dto.UserDto;
+import com.demo.demo.services.UsersService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
 public class UsersController {
+    final private UsersService userSvc;
+
+    UsersController(UsersService userSvc){
+        this.userSvc = userSvc;
+    }
+
+    @GetMapping("/getAllUsers")
+    public List<UserDto> getUsers(){
+        return userSvc.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(userSvc.getUserById(id));
+    }
+
+    @PostMapping()
+    public ResponseEntity<UserDto> createUser(@RequestBody CreateUserDTO user){
+        return ResponseEntity.status(HttpStatus.CREATED).body(userSvc.createUser(user));
+    }
 }
