1eb5cd7f68d0b7591447825528e183b5abb93503
MOHD Zeeshan Quraishi <zq8281@gmail.com>
2026-07-21 19:43:14 +0530

    Another DTO for creating new users

---
+++ src/main/java/com/demo/demo/dto/CreateUserDTO.java
@@ -0,0 +1,27 @@
+package com.demo.demo.dto;
+
+public class CreateUserDTO {
+    private String name;
+    private String email;
+
+    public CreateUserDTO(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String email() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
