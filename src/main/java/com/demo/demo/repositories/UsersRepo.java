8dd0ea56e83c2d310cfdab8e56e713eac8c9609f
MOHD Zeeshan Quraishi <zq8281@gmail.com>
2026-07-21 19:45:31 +0530

    Functions added to find user by id and to create new users

--- src/main/java/com/demo/demo/repositories/UsersRepo.java
+++ src/main/java/com/demo/demo/repositories/UsersRepo.java
@@ -1,5 +1,6 @@
 package com.demo.demo.repositories;
 
+import com.demo.demo.dto.CreateUserDTO;
 import com.demo.demo.dto.UserDto;
 import org.springframework.stereotype.Repository;
 
@@ -10,12 +11,27 @@ public class UsersRepo {
     List<UserDto> users = new ArrayList<>();
 
     public UsersRepo(){
-        users.add(new UserDto("salama101", "zq8281@gmail.com", "Zeeshan"));
-        users.add(new UserDto("lalua202", "lallilal@gmail.com", "Lalu"));
-        users.add(new UserDto("chattis36", "kamles@gmail.com", "Kamal Singh"));
+        users.add(new UserDto(1234, "zq8281@gmail.com", "Zeeshan"));
+        users.add(new UserDto(5678, "lallilal@gmail.com", "Lalu"));
+        users.add(new UserDto(9101112, "kamles@gmail.com", "Kamal Singh"));
     }
 
     public List<UserDto> findAll(){
         return users;
+    }
+
+    public UserDto findById(Long id){
+        for(UserDto user: users){
+            if(user.getId() == id){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public UserDto create(CreateUserDTO user){
+        UserDto newUser = new UserDto((long)Math.random()*100, user.name(), user.email());
+        users.add(newUser);
+        return newUser;
     }
 }
