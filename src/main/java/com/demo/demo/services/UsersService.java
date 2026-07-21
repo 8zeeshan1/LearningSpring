6d0e5901d15e932d3fc3d4a476f79b6bdfaa6f8a
MOHD Zeeshan Quraishi <zq8281@gmail.com>
2026-07-21 19:46:28 +0530

    Added service functionalities

--- src/main/java/com/demo/demo/services/UsersService.java
+++ src/main/java/com/demo/demo/services/UsersService.java
@@ -1,5 +1,6 @@
 package com.demo.demo.services;
 
+import com.demo.demo.dto.CreateUserDTO;
 import com.demo.demo.dto.UserDto;
 import com.demo.demo.repositories.UsersRepo;
 import org.springframework.stereotype.Service;
@@ -15,5 +16,13 @@ public class UsersService {
 
     public List<UserDto> getAllUsers(){
         return repo.findAll();
+    }
+
+    public UserDto getUserById(Long id){
+        return repo.findById(id);
+    }
+
+    public UserDto createUser(CreateUserDTO user){
+        return repo.create(user);
     }
 }
